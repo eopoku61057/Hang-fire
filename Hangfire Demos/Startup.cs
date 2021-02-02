e94522136e030278c36e3ea4660311f70f0c53f3
emmanuelopoku <emmanuel.opoku@trojans.dsu.edu>
2021-02-02 10:51:43 -0600

    adding hang fire original files

---
+++ Hangfire Demos/Startup.cs
@@ -0,0 +1,67 @@
+
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Hangfire_Demos
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(Configure =>
+                Configure.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseDefaultTypeSerializer()
+                .UseMemoryStorage());
+
+            services.AddHangfireServer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IWebHostEnvironment env,
+            IBackgroundJobClient backgroundJobClient,
+            IRecurringJobManager recurringJobManager
+            )
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.UseHangfireDashboard();
+
+            backgroundJobClient.Enqueue(() => Console.WriteLine("Hello World, Welcome to HANGFIRE"));
+
+            backgroundJobClient.Enqueue(() => Test());
+            recurringJobManager.AddOrUpdate(
+                "Run every minute",
+                () => Console.WriteLine("Test recurring job"),
+                "* * * * *"
+                );
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
